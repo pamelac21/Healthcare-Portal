8a75fda94c36726aa5114fbf1716def7caed060e
PamelaCleveland <pamelac021@gmail.com>
2022-01-04 01:59:59 -0500

    auth

---
+++ server/config/seeds.js
@@ -0,0 +1,13 @@
+const db = require('./connection');
+const { Users, Facilities, Procedures, Providers } = require('../models');
+
+db.once('open', async () => {
+
+
+
+
+
+})
+
+
+console.log('products seeded');
