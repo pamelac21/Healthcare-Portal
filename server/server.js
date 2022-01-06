c6716380ea2697502825cb7e8d4f4ab3cff1c456
PamelaCleveland <pamelac021@gmail.com>
2022-01-05 20:14:37 -0500

    index v. server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const dotenv = require("dotenv");//
+//const mongoose = require("mongoose");//
 const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
@@ -7,6 +9,7 @@ const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+dotenv.config();//
 const app = express();
 const server = new ApolloServer({
   typeDefs,
